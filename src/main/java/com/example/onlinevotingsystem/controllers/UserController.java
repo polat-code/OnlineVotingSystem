e991af3290eaa2935251f64ccbee81c427097400
ozgurhanpolat <ozgurhan.45@gmail.com>
2023-05-17 21:16:06 +0300

    Packages is created for Data Access Objects (Dto). Also UserController is created for user request and responds

--- src/main/java/com/example/onlinevotingsystem/controllers/UserController.java
+++ src/main/java/com/example/onlinevotingsystem/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.example.onlinevotingsystem.controllers;
 
+import com.example.onlinevotingsystem.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
 
 
 }
