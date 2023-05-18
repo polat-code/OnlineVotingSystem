6e4a5c9ca60fa006c960d270053727f52ef099c5
Bugra <71045060+TheGnc@users.noreply.github.com>
2023-05-18 17:18:49 +0300

    feat  : do not use ( _ ) because when we sent any query getting data from db . The query does not work , so election_id  => electionId etc ..

--- src/main/java/com/example/onlinevotingsystem/models/Faculty.java
+++ src/main/java/com/example/onlinevotingsystem/models/Faculty.java
@@ -13,7 +13,7 @@ public class Faculty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "faculty_id", nullable = false)
-    private Long id;
+    private Long facultyId;
 
 
 
