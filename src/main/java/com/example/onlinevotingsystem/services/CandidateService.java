416e3be256a2970e6a5a82295850e7b61ff3b8b3
ozgurhanpolat <ozgurhan.45@gmail.com>
2023-06-02 00:58:12 +0300

    When application is approved , Candidate is added to candidates_for_election table

--- src/main/java/com/example/onlinevotingsystem/services/CandidateService.java
+++ src/main/java/com/example/onlinevotingsystem/services/CandidateService.java
@@ -3,11 +3,17 @@ package com.example.onlinevotingsystem.services;
 import com.example.onlinevotingsystem.Dto.requests.CreateCandidateRequest;
 import com.example.onlinevotingsystem.Dto.responses.CandidateGetResponse;
 import com.example.onlinevotingsystem.models.Candidate;
+import com.example.onlinevotingsystem.models.Department;
+import com.example.onlinevotingsystem.models.Election;
 import com.example.onlinevotingsystem.models.Student;
 import com.example.onlinevotingsystem.repository.CandidateRepository;
+import com.example.onlinevotingsystem.repository.ElectionRepository;
 import com.example.onlinevotingsystem.repository.StudentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +23,8 @@ public class CandidateService {
 
     private StudentRepository studentRepository;
 
+    private ElectionRepository electionRepository;
+
 
 
 
@@ -25,10 +33,24 @@ public class CandidateService {
         Candidate candidate = new Candidate().builder()
                 .student(student)
                 .isCanceled(false)
+                .elections(new ArrayList<>())
                 .build();
-        // Write a code to add Candidate Into candidate_for_election table.
 
         candidateRepository.save(candidate);
+        // Write a code to add Candidate Into candidate_for_election table.
+        
+        Election election = findElectionIdByUserId(student);
+        election.getCandidates().add(candidate);
+        candidate.getElections().add(election);
+        candidateRepository.save(candidate);
+        electionRepository.save(election);
+
+    }
+
+    private Election findElectionIdByUserId(Student student) {
+        Department department = student.getDepartment();
+        Election election = electionRepository.findByDepartmentId(student.getDepartment().getDepartmentId()).get();
+        return election;
     }
 
 
