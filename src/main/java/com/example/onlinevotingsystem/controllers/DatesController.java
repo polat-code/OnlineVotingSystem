6b9b97d957a1deb1f333d3923b04c3e571695564
ozgurhanpolat <ozgurhan.45@gmail.com>
2023-06-08 00:36:39 +0300

    Dates is updated

--- src/main/java/com/example/onlinevotingsystem/controllers/DatesController.java
+++ src/main/java/com/example/onlinevotingsystem/controllers/DatesController.java
@@ -7,26 +7,24 @@ import com.example.onlinevotingsystem.services.DatesService;
 import com.example.onlinevotingsystem.services.ElectionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/dates")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DatesController {
 
     private DatesService datesService;
 
     private ElectionService electionService;
-    @PostMapping("dates/application")
+    @PostMapping("/application")
     public ResponseEntity<Object> setDatesOfApplication(@RequestBody ApplicationDatesRequest datesRequest) {
         return  this.datesService.setDatesOfApplication(datesRequest);
 
     }
 
-    @PostMapping("dates/election")
+    @PostMapping("/election")
     public void addElection(@RequestBody CreateElectionRequest createElectionRequest){
         this.electionService.addElection(createElectionRequest);
     }
